4f6251cb286e70aa34ffbca71442e03827277090
Darren <darrenanco@Darrens-MBP-2.cable.rcn.com>
2020-05-04 06:13:12 -0400

    Formatting

--- AI.sv
+++ AI.sv
@@ -22,29 +22,29 @@ module AI(input logic Clk,
   logic [7:0] best_accuracy, best_accuracy_in;
 
   logic [1:0] move_in;
-  
+
   logic done_1, done_2, done_3, done_4;
   logic done_1_in, done_2_in, done_3_in, done_4_in;
 
   always_ff @ (posedge Clk)
   begin
     if(Reset)
-	 begin
+    begin
       State <= Begin;
       move <= 2'b0;
-	end
+    end
     else
-	 begin
+  	 begin
       State <= Next_state;
       move <= move_in;
-		best_move <= best_move_in;
-		best_damage <= best_damage_in;
-		best_accuracy <= best_accuracy_in;
-		done_1 <= done_1_in;
-		done_2 <= done_2_in;
-		done_3 <= done_3_in;
-		done_4 <= done_4_in;
-	end
+  		best_move <= best_move_in;
+  		best_damage <= best_damage_in;
+  		best_accuracy <= best_accuracy_in;
+  		done_1 <= done_1_in;
+  		done_2 <= done_2_in;
+  		done_3 <= done_3_in;
+  		done_4 <= done_4_in;
+  	end
   end
 
   always_comb
@@ -56,11 +56,11 @@ module AI(input logic Clk,
     player_hp_per = (player_hp * 100) / player_max_hp;
 
     move_in = move;
-	 
+
 	 best_move_in = best_move;
 	 best_damage_in = best_damage;
 	 best_accuracy_in = best_accuracy;
-	 
+
 	 done_1_in = done_1;
 	 done_2_in = done_2;
 	 done_3_in = done_3;
@@ -97,115 +97,115 @@ module AI(input logic Clk,
     case(State)
       Begin:
       begin
-		  done_1_in = 1'b0;
-		  done_2_in = 1'b0;
-		  done_3_in = 1'b0;
-		  done_4_in = 1'b0;
+  		  done_1_in = 1'b0;
+  		  done_2_in = 1'b0;
+  		  done_3_in = 1'b0;
+  		  done_4_in = 1'b0;
       end
       Load_1:
         move_in = 2'b0;
-		Move_1:
-		begin
-			 best_move_in = move;
-			 best_damage_in = damage;
-			 best_accuracy_in = move_data[1];
-			 done_1_in = 1'b1;
-		end
+  		Move_1:
+  		begin
+  			 best_move_in = move;
+  			 best_damage_in = damage;
+  			 best_accuracy_in = move_data[1];
+  			 done_1_in = 1'b1;
+  		end
       Load_2:
         move_in = 2'b01;
-		Move_2:
-		begin
-			 if(player_hp_per < 20 || CPU_hp_per < 20)
-			 begin
-				if(move_data[1] >= best_accuracy && damage > best_damage)
-				begin
-				  best_move_in = move;
-				  best_damage_in = damage;
-				  best_accuracy_in = move_data[1];
-				end
-			 end
-			 else if(CPU_hp_per >= 90)
-			 begin
-				if(damage > best_damage)
-				begin
-				  best_move_in = move;
-				  best_damage_in = damage;
-				  best_accuracy_in = move_data[1];
-				end
-			 end
-			 else if(((damage*move_data[1]) / 100) > ((best_damage*best_accuracy) / 100))
-				begin
-				  best_move_in = move;
-				  best_damage_in = damage;
-				  best_accuracy_in = move_data[1];
-				end
-			 done_2_in = 1'b1;
-		end
+  		Move_2:
+  		begin
+  			 if(player_hp_per < 20 || CPU_hp_per < 20)
+  			 begin
+  				if(move_data[1] >= best_accuracy && damage > best_damage)
+  				begin
+  				  best_move_in = move;
+  				  best_damage_in = damage;
+  				  best_accuracy_in = move_data[1];
+  				end
+  			 end
+  			 else if(CPU_hp_per >= 90)
+  			 begin
+  				if(damage > best_damage)
+  				begin
+  				  best_move_in = move;
+  				  best_damage_in = damage;
+  				  best_accuracy_in = move_data[1];
+  				end
+  			 end
+  			 else if(((damage*move_data[1]) / 100) > ((best_damage*best_accuracy) / 100))
+  				begin
+  				  best_move_in = move;
+  				  best_damage_in = damage;
+  				  best_accuracy_in = move_data[1];
+  				end
+  			 done_2_in = 1'b1;
+  		end
       Load_3:
         move_in = 2'b10;
-		Move_3:
-		begin
-			 if(player_hp_per < 20 || CPU_hp_per < 20)
-			 begin
-				if(move_data[1] >= best_accuracy && damage > best_damage)
-				begin
-				  best_move_in = move;
-				  best_damage_in = damage;
-				  best_accuracy_in = move_data[1];
-				end
-			 end
-			 else if(CPU_hp_per >= 90)
-			 begin
-				if(damage > best_damage)
-				begin
-				  best_move_in = move;
-				  best_damage_in = damage;
-				  best_accuracy_in = move_data[1];
-				end
-			 end
-			 else if(((damage*move_data[1]) / 100) > ((best_damage*best_accuracy) / 100))
-				begin
-				  best_move_in = move;
-				  best_damage_in = damage;
-				  best_accuracy_in = move_data[1];
-				end
-			 done_3_in = 1'b1;
-		end
+  		Move_3:
+  		begin
+  			 if(player_hp_per < 20 || CPU_hp_per < 20)
+  			 begin
+  				if(move_data[1] >= best_accuracy && damage > best_damage)
+  				begin
+  				  best_move_in = move;
+  				  best_damage_in = damage;
+  				  best_accuracy_in = move_data[1];
+  				end
+  			 end
+  			 else if(CPU_hp_per >= 90)
+  			 begin
+  				if(damage > best_damage)
+  				begin
+  				  best_move_in = move;
+  				  best_damage_in = damage;
+  				  best_accuracy_in = move_data[1];
+  				end
+  			 end
+  			 else if(((damage*move_data[1]) / 100) > ((best_damage*best_accuracy) / 100))
+  				begin
+  				  best_move_in = move;
+  				  best_damage_in = damage;
+  				  best_accuracy_in = move_data[1];
+  				end
+  			 done_3_in = 1'b1;
+  		end
       Load_4:
         move_in = 2'b11;
-		Move_4:
-		begin
-			 if(player_hp_per < 20 || CPU_hp_per < 20)
-			 begin
-				if(move_data[1] >= best_accuracy && damage > best_damage)
-				begin
-				  best_move_in = move;
-				  best_damage_in = damage;
-				  best_accuracy_in = move_data[1];
-				end
-			 end
-			 else if(CPU_hp_per >= 90)
-			 begin
-				if(damage > best_damage)
-				begin
-				  best_move_in = move;
-				  best_damage_in = damage;
-				  best_accuracy_in = move_data[1];
-				end
-			 end
-			 else if(((damage*move_data[1]) / 100) > ((best_damage*best_accuracy) / 100))
-				begin
-				  best_move_in = move;
-				  best_damage_in = damage;
-				  best_accuracy_in = move_data[1];
-				end
-			 done_4_in = 1'b1;
-		end
+  		Move_4:
+  		begin
+  			 if(player_hp_per < 20 || CPU_hp_per < 20)
+  			 begin
+  				if(move_data[1] >= best_accuracy && damage > best_damage)
+  				begin
+  				  best_move_in = move;
+  				  best_damage_in = damage;
+  				  best_accuracy_in = move_data[1];
+  				end
+  			 end
+  			 else if(CPU_hp_per >= 90)
+  			 begin
+  				if(damage > best_damage)
+  				begin
+  				  best_move_in = move;
+  				  best_damage_in = damage;
+  				  best_accuracy_in = move_data[1];
+  				end
+  			 end
+  			 else if(((damage*move_data[1]) / 100) > ((best_damage*best_accuracy) / 100))
+  				begin
+  				  best_move_in = move;
+  				  best_damage_in = damage;
+  				  best_accuracy_in = move_data[1];
+  				end
+  			 done_4_in = 1'b1;
+  		end
       Done:
-		begin
+  		begin
         move_in = best_move;
         CPU_done = 1'b1;
-		end
+  		end
     endcase
   end
 
